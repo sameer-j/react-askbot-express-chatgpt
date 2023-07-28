3d891d759313051eb9a14a2af6e8ebc54f8461e3
sameer-j <samir4nov@gmail.com>
2023-07-28 11:44:00 +0530

    web ready

--- askbot-web/src/App.tsx
+++ askbot-web/src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import lens from "./assets/lens.png";
+import loadingGif from "./assets/loading.gif";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [prompt, updatePrompt] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [answer, setAnswer] = useState<string>("");
+
+  useEffect(() => {
+    if (prompt != null && prompt.trim() === "") {
+      setAnswer("");
+    }
+  }, [prompt]);
+
+  const sendPrompt = async (event: React.KeyboardEvent) => {
+    if (event.key !== "Enter") {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      setAnswer("");
+      const response = await fetch("http://localhost:2000/aiCompletion", {
+        method: "post",
+        headers: {
+          Accept: "application/json, text/plain, */*", // indicates which files we are able to understand
+          "Content-Type": "application/json", // indicates what the server actually sent
+        },
+        body: JSON.stringify({ userPrompt: prompt }), // server is expecting JSON
+      });
+      if (!response.ok || !response.body) {
+        throw response.statusText;
+      }
+      const reader = response.body.getReader();
+      const decoder = new TextDecoder();
+      const loopRunner = true;
+
+      while (loopRunner) {
+        const { value, done } = await reader.read();
+        if (done) {
+          break;
+        }
+        const decodedChunk = decoder.decode(value, { stream: true });
+        setAnswer((answer) => answer + decodedChunk);
+      }
+    } catch (err) {
+      console.error(err, "err");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="app-container">
+        <div className="spotlight__wrapper">
+          <input
+            type="text"
+            className="spotlight__input"
+            placeholder="Ask me anything..."
+            disabled={loading}
+            style={{
+              backgroundImage: loading ? `url(${loadingGif})` : `url(${lens})`,
+            }}
+            onChange={(e) => updatePrompt(e.target.value)}
+            onKeyDown={(e) => sendPrompt(e)}
+          />
+          <div className="spotlight__answer">{answer && <p>{answer}</p>}</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
